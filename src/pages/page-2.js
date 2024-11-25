ea8c2d68d42b234cc1635e4831648d14d7dd4e30
laijingchu <laijing.chu@gmail.com>
2024-11-24 21:12:37 -0500

    first

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,7 +6,7 @@ import Seo from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
+    <h1>Just changing some stuff around!</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
